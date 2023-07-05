3e2b0973fbc0bebe1e6bc079b3ed69deb53589d4
Saugata <saugataroyarghya@gmail.com>
2023-07-06 03:30:46 +0600

    Forget password page

--- app/src/main/java/com/example/cardiac_recorder/Forget_password.java
+++ app/src/main/java/com/example/cardiac_recorder/Forget_password.java
@@ -3,9 +3,21 @@ package com.example.cardiac_recorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Forget_password extends AppCompatActivity {
 
+    private Button Buttonforget;
+    private EditText email;
+    private ProgressBar progressBar;
+    private TextView Exit;
+
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
